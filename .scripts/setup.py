4f533c9308dddced0676df8bd02a03a9438cfc11
MoOT <mathieu.chataigner@gmail.com>
2012-11-07 22:22:30 +0100

    setup.py auto

--- .scripts/setup.py
+++ .scripts/setup.py
@@ -2,29 +2,54 @@
 
 import os,subprocess as sub
 
+
 home = os.environ['HOME']+os.sep
 
 if not os.path.isdir(home+'usr'):
 	os.mkdir(home+'usr')
 
-os.chdir(home+'usr')
+os.chdir(home)
 
-f = open(home+'.git_repositories')
+def depend():
+	dependencies = ['git','git-core','wget','encfs']
+	sub.call(['sudo','apt-get','install']+dependencies)
 
-for i in f.readlines():
-	cur = os.getcwd()
-	if os.path.isdir(cur+os.sep+i.strip()):
+def git_init():
+	sub.call(['git','clone','https://github.com/mootcube/config.git'])
+	sub.call(['rsync','-av','~/config/','~/'])
+	sub.call(['rm','-rf','~/config'])
+
+def install_packages():
+	if os.path.isfile(home+'.packages'):
+		f = open(home+'.packages')
+		packages = list(map(lambda i: i.strip(),f.readlines()))
+		f.close()
+		sub.call(['sudo','apt-get','update'])
+		sub.call(['sudo','apt-get','install']+packages)
+
+def git_repo():
+	os.chdir(home+'usr')
+	f = open(home+'.git_repositories')
+
+	for i in f.readlines():
+		cur = os.getcwd()
+		if os.path.isdir(cur+os.sep+i.strip()):
+			os.chdir(cur+os.sep+i.strip())
+			sub.call(['git','pull'])
+		else:
+			sub.call(['git','clone','git@github.com:mootcube/'+i.strip()+'.git'])
 		os.chdir(cur+os.sep+i.strip())
-		sub.call(['git','pull'])
-	else:
-		sub.call(['git','clone','git@github.com:mootcube/'+i.strip()+'.git'])
-	os.chdir(cur+os.sep+i.strip())
-	if os.path.isfile('dependencies'):
-		d=open('dependencies')
-		a=d.readlines()
-		d.close()
-		dep = list(map(lambda i: i.strip(),a))
-		sub.call(['sudo','apt-get','install']+dep)
-	sub.call(['python','setup.py'])
-	os.chdir(cur)
-f.close()
+		if os.path.isfile('dependencies'):
+			d=open('dependencies')
+			a=d.readlines()
+			d.close()
+			dep = list(map(lambda i: i.strip(),a))
+			sub.call(['sudo','apt-get','install']+dep)
+		sub.call(['python','setup.py'])
+		os.chdir(cur)
+	f.close()
+
+depend()
+git_init()
+install_packages()
+git_repo()
