81e65de0b07636a2eafe1e125f9602e598c02d6b
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2013-03-05 18:19:29 +0100

    Update install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -11,3 +11,15 @@ git clone --recursive https://github.com/mchataigner/config.git .mathconfig
 rsync -av .mathconfig/ $HOME
 
 rm -rf .mathconfig
+
+if [ -e ~/.ssh/id_rsa.pub ];then
+  echo "ssh key already exists"
+else
+  read -p "What is your email?" mail
+
+  ssh-keygen -t rsa -C $mail
+fi
+
+sudo apt-get install xclip
+
+xclip -sel clip < ~/.ssh/id_rsa.pub
