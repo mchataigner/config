7d6015fa9900a8a7d32b44952c8314e0079cbb0b
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2013-03-05 17:25:14 +0100

    Update install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+cd $HOME
+
+wget --no-check-certificate https://github.com/mchataigner/config/raw/master/.gitconfig
+
 git clone https://github.com/mchataigner/config.git .mathconfig
 
 mv .mathconfig/* $HOME
 mv .mathconfig/.* $HOME
 
-
+rm -rf .mathconfig
